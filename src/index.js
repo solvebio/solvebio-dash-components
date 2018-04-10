1a4299828be5dfc49a729094fc6bd50069c81d8e
David Park <dpark@solvebio.com>
2018-04-10 11:35:41 -0400

    added DashPackageLoader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/prefer-default-export */
+import DashPackageLoader from './components/DashPackageLoader.react';
 import DashS3Uploader from './components/DashS3Uploader.react';
 import QueryBuilder from './components/QueryBuilder.react';
 
 export {
+    DashPackageLoader,
     DashS3Uploader,
     QueryBuilder
 };
