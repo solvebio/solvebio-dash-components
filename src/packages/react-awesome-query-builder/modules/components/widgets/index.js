848c5a01ef5ec0ff10ec9e6c58075217cef2ca5f
David Park <dpark@solvebio.com>
2018-04-10 11:33:05 -0400

    removed all non-build files, added NOT function to react-awesome-query-builder component

--- src/packages/react-awesome-query-builder/modules/components/widgets/index.js
+++
@@ -1,9 +0,0 @@
-export {default as DateWidget} from './Date';
-export {default as DateTimeWidget} from './DateTime'
-export {default as TimeWidget} from './Time'
-export {default as SelectWidget} from './Select';
-export {default as TextWidget} from './Text';
-export {default as NumberWidget} from './Number';
-export {default as BooleanWidget} from './Boolean'
-export {default as MultiSelectWidget} from './MultiSelect';
-export {default as ValueFieldWidget} from './ValueField';
