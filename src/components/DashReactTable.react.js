e3e62b5d5dcf696dc7db4f22377f22ef34b5e7c6
David Park <dpark@solvebio.com>
2018-05-24 13:28:11 -0400

    bump pypi package version v0.2.6

---
+++ src/components/DashReactTable.react.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { makeData, Logo, Tips } from "./DashReactTable.config";
+
+import ReactTable from 'react-table';
+
+import 'react-table/react-table.css';
+
+class DashReactTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {selected: null};
+    this.getTrProps = this.getTrProps.bind(this);
+  }
+
+  insertLinks(key, value) {
+    return (key === 'Cell' && value === 'url')
+      ? row => <a href={row.column.url + row.value} target='_blank'>{row.column.label}</a>
+      : value
+  }
+
+  getTrProps(state, rowInfo) {
+    return {
+      onClick: (e) => {
+        this.setState({
+          selected: rowInfo.index === this.state.selected ? null : rowInfo.index
+        })
+      },
+      style: {
+        background: rowInfo.index === this.state.selected ? 'rgb(144,238,144,.6)' : 'inherit',
+      }
+    };
+  }
+
+  render() {
+    const data = JSON.parse(this.props.data)
+    const columns = JSON.parse(this.props.columns, this.insertLinks);
+    const tableProps = { getTrProps: this.getTrProps };
+
+    return (
+      <div>
+        <ReactTable
+          {...tableProps}
+          data={data}
+          columns={columns}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+      </div>
+    );
+  }
+
+}
+
+DashReactTable.propTypes = {
+  id: PropTypes.string,
+  data: PropTypes.string,
+  columns: PropTypes.string
+};
+
+export default DashReactTable;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import DashPackageLoader from './components/DashPackageLoader.react';
+import DashReactTable from './components/DashReactTable.react';
 import DashS3Uploader from './components/DashS3Uploader.react';
 import QueryBuilder from './components/QueryBuilder.react';
 
 export {
     DashPackageLoader,
+    DashReactTable,
     DashS3Uploader,
     QueryBuilder
 };
