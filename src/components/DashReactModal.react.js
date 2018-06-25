42b82b00fd8b815b284bbac275e18c55a13a578b
David Park <dpark@solvebio.com>
2018-06-25 19:31:11 -0400

    v0.3.0 added DashReactModal component

---
+++ src/components/DashReactModal.react.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import ReactModal from 'react-modal';
+
+class DashReactModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  handleCloseModal() {
+    this.props.setProps({
+      isOpen: false
+    });
+  }
+
+  render() {
+    const modalHeight = this.state.height * 0.8;
+    return (
+      <div>
+        <ReactModal
+          isOpen={this.props.isOpen}
+          ariaHideApp={false}
+          onRequestClose={this.handleCloseModal}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.8)'
+            },
+            content: {
+              top: '50%',
+              left: '50%',
+              right: 'auto',
+              bottom: 'auto',
+              marginRight: '-50%',
+              transform: 'translate(-50%, -50%)',
+              padding: '0px',
+              height: `${modalHeight}px`
+            }
+          }}
+          {...this.props}
+        >
+          {this.props.children}
+        </ReactModal>
+      </div>
+    );
+  }
+}
+
+DashReactModal.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  isOpen: PropTypes.bool
+};
+
+DashReactModal.defaultProps = {
+  isOpen: false
+};
+
+export default DashReactModal;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import DashPackageLoader from './components/DashPackageLoader.react';
+import DashReactModal from './components/DashReactModal.react';
 import DashReactTable from './components/DashReactTable.react';
 import DashS3Uploader from './components/DashS3Uploader.react';
 import QueryBuilder from './components/QueryBuilder.react';
 
 export {
     DashPackageLoader,
+    DashReactModal,
     DashReactTable,
     DashS3Uploader,
     QueryBuilder
