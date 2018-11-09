2fa5358ff17229d3e5295053a1c1095fed47e5db
David Park <dpark@solvebio.com>
2018-11-09 12:23:43 -0500

    modify example dash react table

--- dash_react_table.py
+++ dash_react_table.py
@@ -1,4 +1,8 @@
 import json
+import os
+import uuid
+
+from flask import send_from_directory
 
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
@@ -11,17 +15,21 @@ app = SolveBioDash(
     name=__name__,
     title='Dash React Table',
     app_url='http://127.0.0.1:8050',
-    client_id='0lxt2cuv78f9a2zynlpqgxoc3o1pgb4whwd5z5wk')
+    client_id='0lxt2cuv78f9a2zynlpqgxoc3o1pgb4whwd5z5wk',
+    salt='dash_react_table')
 
 columns = [{'Header': 'A',
-            'accessor': 'a'},
+            'accessor': 'a',
+            'Cell': 'comments',
+            'commentsAccessor': 'comments',
+            'commentsUrlAccessor': 'commentsUrl'},
            {'Header': 'B',
             'accessor': 'b'},
            {'Header': 'C',
             'accessor': 'c'}]
 
-data = [{'a': '1', 'b': '2', 'c': '3'},
-        {'a': '2', 'b': '3', 'c': '1'},
+data = [{'a': '1', 'b': '2', 'c': '3', 'comments': ['tag1', 'tag2'], 'commentsUrl': 'comments?variant=SPOP'},
+        {'a': '2', 'b': '3', 'c': '1', 'comments': ['tagA', 'tagB'], 'commentsUrl': 'comments?variant=KRAS'},
         {'a': '3', 'b': '1', 'c': '2'}]
 
 sortBy = [{'id': 'c'}]
@@ -33,6 +41,27 @@ app.layout = html.Div(
         columns=json.dumps(columns),
         sortBy=json.dumps(sortBy),
         unknown=False))
+
+app.css.append_css({
+    'external_url': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
+})
+
+app.css.append_css({
+    "external_url":
+    "//solvebio.s3.amazonaws.com/static/v6.6.0-209641abac/mesh/css/style.css"
+})
+
+# Add a UUID cache buster to reload CSS every time
+app.css.append_css({
+    "external_url": "/static/style.css{}"
+    .format('?v={}'.format(uuid.uuid4()))
+})
+
+
+@app.server.route('/static/style.css')
+def serve_stylesheet():
+    assets_dir = os.path.join(os.getcwd(), 'assets')
+    return send_from_directory(assets_dir, 'style.css')
 
 
 @app.callback(
