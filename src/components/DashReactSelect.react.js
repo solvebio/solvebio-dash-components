603d7ad3852f60f69b9e321e7872efd36aa2a28e
David Park <dpark@solvebio.com>
2019-03-12 13:57:15 -0400

    add DashReactSelect component

--- src/components/DashReactSelect.react.js
+++ src/components/DashReactSelect.react.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { Creatable } from 'react-select';
+
 class DashReactSelect extends Component {
-    render() {
-        return <div>Dash React Select</div>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: this.props.value
+    };
+    this.handleCreateOption = this.handleCreateOption.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleCreateOption(newValues) {
+    let values = this.state.values.slice();
+    newValues = newValues.split(',');
+    newValues.forEach(value => {
+      values.push({
+        label: value,
+        value: value
+      });
+    });
+    this.setState({ values: values });
+  }
+
+  handleChange(newValues) {
+    this.setState({ values: newValues });
+  }
+
+  render() {
+    return <Creatable
+      isMulti={this.props.isMulti}
+      options={this.props.options}
+      value={this.state.values}
+      onChange={this.handleChange}
+      onCreateOption={this.handleCreateOption}
+    />;
+  }
 }
 
 DashReactSelect.propTypes = {
-    id: PropTypes.string
+  id: PropTypes.string,
+  isMulti: PropTypes.bool,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string
+    })
+  ),
+  value: PropTypes.oneOfType([
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string
+    }),
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string,
+        value: PropTypes.string
+      })
+    )
+  ])
+};
+
+DashReactSelect.defaultProps = {
+  isMulti: true,
+  options: [],
+  value: []
 };
 
 export default DashReactSelect;
